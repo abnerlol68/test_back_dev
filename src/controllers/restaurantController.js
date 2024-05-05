67eb94c11c15c2a11e7c89facbd2174255499b5f
Abner Perez Rodriguez <abnerstr81@gmail.com>
2024-05-04 20:45:52 -0600

    Create controllers and services for end-points (CRUD)

--- src/controllers/restaurantController.js
+++ src/controllers/restaurantController.js
@@ -1,3 +1,4 @@
+const { v4: uuid } = require("uuid");
 const restaurantService = require("../services/restaurantService");
 
 const getAllRestaurants = async (_req, res) => {
@@ -6,28 +7,150 @@ const getAllRestaurants = async (_req, res) => {
     res.json({ message: "Restaurants info", data: allRestaurants });
   } catch (err) {
     console.error(err);
-    res.status(400).json({ message: "Restaurants not found" });
+    return res.status(400).json({ message: "Restaurants not found" });
   }
 };
 
-const getOneRestaurant = (req, res) => {
-  const restaurant = restaurantService.getOneRestaurant();
-  res.send("Get an existing Restaurant");
+const getOneRestaurant = async (req, res) => {
+  try {
+    const { restaurantId } = req.params;
+    const restaurant = (
+      await restaurantService.getOneRestaurant(restaurantId)
+    )[0];
+
+    if (!restaurant.length) {
+      return res.status(404).json({ message: "Restaurant not found" });
+    }
+
+    res.json({ message: "Restaurant info", data: restaurant });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
 };
 
-const createNewRestaurant = (req, res) => {
-  const createRestaurant = restaurantService.createNewRestaurant();
-  res.send("Create a new Restaurant");
+const createNewRestaurant = async (req, res) => {
+  try {
+    const { body } = req;
+
+    if (
+      !body.rating ||
+      !body.name ||
+      !body.site ||
+      !body.email ||
+      !body.phone ||
+      !body.street ||
+      !body.city ||
+      !body.state ||
+      !body.lat ||
+      !body.lng
+    ) {
+      res.status(400).json({
+        message:
+          "One of the following keys is missing or is empty in request body: id, rating, name, site, email, phone, street, city, state, lat, lng",
+      });
+    }
+
+    const restaurantFound = await restaurantService.findPreviousRestaurant(
+      body
+    );
+
+    if (restaurantFound[0].length) {
+      return res.status(500).json({
+        message: "Restaurant already exist",
+      });
+    }
+
+    const { affectedRows } = (
+      await restaurantService.createNewRestaurant({
+        id: uuid(),
+        ...structuredClone(body),
+      })
+    )[0];
+
+    if (affectedRows !== 1) {
+      return res.status(400).json({
+        message: "Cannot register new restaurant",
+      });
+    }
+
+    res.json({
+      message: "New restaurant registered",
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
 };
 
-const updateOneRestaurant = (req, res) => {
-  const updateRestaurant = restaurantService.updateOneRestaurant();
-  res.send("Update an existing Restaurant");
+const updateOneRestaurant = async (req, res) => {
+  try {
+    const { restaurantId } = req.params;
+    const { body } = req;
+
+    const restaurantFound = await restaurantService.getOneRestaurant(
+      restaurantId
+    );
+
+    if (!restaurantFound.length) {
+      return res.status(500).json({
+        message: "Restaurant does not exist",
+      });
+    }
+
+    const { affectedRows } = (
+      await restaurantService.updateOneRestaurant(
+        restaurantId,
+        structuredClone(body)
+      )
+    )[0];
+
+    if (affectedRows !== 1) {
+      return res.status(400).json({
+        message: "Cannot register new restaurant",
+      });
+    }
+
+    res.json({
+      message: "Restaurant updated",
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
 };
 
-const deleteOneRestaurant = (req, res) => {
-  const delateRestaurant = restaurantService.deleteOneRestaurant();
-  res.send("Delete an existing Restaurant");
+const deleteOneRestaurant = async (req, res) => {
+  try {
+    const { restaurantId } = req.params;
+
+    const restaurantFound = await restaurantService.getOneRestaurant(
+      restaurantId
+    );
+
+    if (!restaurantFound.length) {
+      return res.status(500).json({
+        message: "Restaurant does not exist",
+      });
+    }
+
+    const { affectedRows } = (
+      await restaurantService.deleteOneRestaurant(restaurantId)
+    )[0];
+
+    if (affectedRows !== 1) {
+      return res.status(400).json({
+        message: "Cannot delete restaurant",
+      });
+    }
+
+    res.json({
+      message: "Restaurant deleted",
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
 };
 
 module.exports = {
--- src/services/restaurantService.js
+++ src/services/restaurantService.js
@@ -9,25 +9,79 @@ const getAllRestaurants = async () => {
   }
 };
 
-const getOneRestaurant = () => {
-  return;
+const getOneRestaurant = async (restaurantId) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM restaurants WHERE id = ?", [
+      restaurantId,
+    ]);
+    return [rows];
+  } catch (err) {
+    throw err;
+  }
 };
 
-const createNewRestaurant = () => {
-  return;
+const findPreviousRestaurant = async (restaurant) => {
+  try {
+    const [rows] = await pool.query(
+      "SELECT * FROM restaurants WHERE rating = ? AND name = ? AND site = ? AND email = ? AND phone = ? AND street = ? AND city = ? AND state = ? AND lat = ? AND lng = ?",
+      Object.values(restaurant)
+    );
+    return [rows];
+  } catch (err) {
+    throw err;
+  }
 };
 
-const updateOneRestaurant = () => {
-  return;
+const createNewRestaurant = async (restaurant) => {
+  try {
+    const req = await pool.query(
+      "INSERT INTO restaurants VALUES (?,?,?,?,?,?,?,?,?,?,?)",
+      Object.values(restaurant)
+    );
+    return req;
+  } catch (err) {
+    throw err;
+  }
 };
 
-const deleteOneRestaurant = () => {
-  return;
+const updateOneRestaurant = async (restaurantId, restaurant) => {
+  try {
+    const req = await pool.query(
+      `UPDATE restaurants SET 
+      rating = IFNULL(?, rating),
+      name = IFNULL(?, name),
+      site = IFNULL(?, site),
+      email = IFNULL(?, email),
+      phone = IFNULL(?, phone),
+      street = IFNULL(?, street),
+      city = IFNULL(?, city),
+      state = IFNULL(?, state),
+      lat = IFNULL(?, lat),
+      lng = IFNULL(?, lng)
+      WHERE id = ?`,
+      [...Object.values(restaurant), restaurantId]
+    );
+    return req;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const deleteOneRestaurant = async (restaurantId) => {
+  try {
+    const req = await pool.query("DELETE FROM restaurants WHERE id = ?", [
+      restaurantId,
+    ]);
+    return req;
+  } catch (err) {
+    throw err;
+  }
 };
 
 module.exports = {
   getAllRestaurants,
   getOneRestaurant,
+  findPreviousRestaurant,
   createNewRestaurant,
   updateOneRestaurant,
   deleteOneRestaurant,
